77f08f558a617f93d39c6fc7d2fec255e822fe6f
Johnson Wang <johnson.wang@northstartechlab.com>
2022-02-17 16:03:23 -0800

    initial commit with chapter 1 project structure

---
+++ chap_01/hello_world/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Hello World Travel",
+      textDirection: TextDirection.ltr,),
+    );
+  }
+
+}
